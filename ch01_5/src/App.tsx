df801c0fd94c20d1dd2cf4d8d99678dc558450df
HyeYeon0526 <hyk4238@naver.com>
2023-03-20 13:23:39 +0900

    3/20

---
+++ ch01_5/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  console.log('APP() 함수 호출됨');
+  return (
+    <div className="App">
+      <h1> 안녕하세요 리액트 </h1>
+    </div>
+  );
+}
+
+export default App;
